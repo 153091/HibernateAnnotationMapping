be9d01ae2c4c56ff7dc7237da2b6ed160055213e
18741847 <AOKulakov@sberbank.ru>
2020-11-11 13:52:12 +0300

    work on Repository

--- src/main/java/repository/EmployeeRepository.java
+++ src/main/java/repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package repository;
 
-public interface EmployeeRepository {
+import entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
